d5b12ad4904e3245017b4fe793ceb254b0c59e98
Valentin <kibirevvalik202@gmail.com>
2020-12-17 17:27:16 +0300

    [update] Building

---
+++ RockClimber/Assets/Scripts/Enemy/EnemyGan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGan : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _shotPos;
+    [SerializeField]
+    private Bullet _bullet;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
