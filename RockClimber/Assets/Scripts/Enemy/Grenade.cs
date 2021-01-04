b505632ff9e583418b14e436563eeb43e09475d1
Valentin <kibirevvalik202@gmail.com>
2021-01-04 15:34:29 +0300

    [update] Enemy sniper [update] Granade

--- RockClimber/Assets/Scripts/Enemy/Grenade.cs
+++ RockClimber/Assets/Scripts/Enemy/Grenade.cs
@@ -29,14 +29,23 @@ public class Grenade : MonoBehaviour
             transform.position = _spline.GetPoint(_pointPos);
             _pointPos += _speed;
         }
-        else if(!_sphereCollider.enabled)
+        else if(_mesh.enabled)
         {
-            _sphereCollider.radius = _explosionRadius;
-            _sphereCollider.enabled = true;
-            _mesh.enabled = false;
-            _explosion.Play();
-            Destroy(gameObject,1f);
+            StartCoroutine(Destroy());
         }
+    }
+    private IEnumerator Destroy()
+    {
+        _sphereCollider.radius = _explosionRadius;
+        _sphereCollider.enabled = true;
+        _mesh.enabled = false;
+        _explosion.Play();
+        yield return new WaitForSeconds(0.7f);
+        _sphereCollider.enabled = false;
+        yield return new WaitForSeconds(0.3f);
+
+        Destroy(gameObject);
+
     }
     private void OnDrawGizmosSelected()
     {
@@ -48,11 +57,11 @@ public class Grenade : MonoBehaviour
         _spline = spline;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //if (other.tag =="Player")
-        //{
-        //    Destroy(other.transform.parent.gameObject);
-        //}
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //if (other.tag =="Player")
+    //    //{
+    //    //    Destroy(other.transform.parent.gameObject);
+    //    //}
+    //}
 }
